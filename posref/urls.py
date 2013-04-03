55fb6b4155f82b4ab5a20ca06c28f9c8031cca7f
Georg Walther <georgwalther@gmail.com>
2013-04-03 22:15:13 +0100

    initial commit

---
+++ posref/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+from registration.views import activate
+from registration.views import register
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
+    url(r'^about/', TemplateView.as_view(template_name="about.html"), name='about'),
+    #url(r'^posref/', include('posref.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    #url(r'^admin/', include(admin.site.urls)),
+    
+    (r'^accounts/', include('allauth.urls')),
+
+)
