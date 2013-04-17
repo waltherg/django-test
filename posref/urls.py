cb617ef96d621cecd8f9b7c239ac9d3eac70ea46
Georg Walther <georgwalther@gmail.com>
2013-04-18 00:17:40 +0100

    userena expressions in url.py

--- posref/urls.py
+++ posref/urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     #url(r'^admin/', include(admin.site.urls)),
-    
-    (r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/', include('userena.urls')),
+    #(r'^accounts/', include('allauth.urls')),
 
 )
